7b754538cdee4741d34c2913836349e25c154c6a
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-18 16:57:28 +0600

    Update Main7.java

--- Task 2/Main7.java
+++ Task 2/Main7.java
@@ -1,1 +1,21 @@
+package com.mycompany.main1;
+ interface Clickable{
+     
+     void onClick();
+ }
 
+class Button implements Clickable {
+    
+    public void onClick(){
+        System.out.println("Button was clicked!");
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        Button button = new Button();
+        
+        button.onClick();
+    }
+ 
+}
