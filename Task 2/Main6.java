bfa0253d11f8d43f24c3909444b2163525a8cda9
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-18 16:56:47 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,24 @@
 
+package com.mycompany.main1;
+
+class Applet{
+    void launch(){
+        System.out.println("Applet Launched");
+    }
+}
+
+class GameApplet extends Applet {
+    
+    void play(){
+        System.out.println("Playing game");
+    }
+}
+
+public class Main6 {
+    public static void main(String[] args) {
+        GameApplet game = new GameApplet();
+        
+        game.launch();
+        game.play();
+    }
+}
