5ce9a563e28b4158f689ae8d483992a763b35102
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-18 16:56:06 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,20 @@
+package com.mycompany.main1;
 
+class Printer{
+    
+    void printData(String text){
+        System.out.println("Text: " + text);  
+    }
+    
+    void printData(int number){
+        System.out.println("Number: " + number);
+    }
+}
+public class Main5 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        
+       printer.printData("Hello, java");
+       printer.printData(100);
+    }
+}
