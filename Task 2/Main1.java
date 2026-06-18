2c5ee796aed175e534930eca5aec00289ba03a8c
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-18 16:12:33 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,17 @@
+package com.mycompany.main1;
 
+class ContestStat{
+    
+    int[] scores ={85, 95, 95};
+    
+    void printFirstScore(){
+        System.out.println("First Score: " + scores[0]);
+    }
+}
+public class Main1 {
+    public static void main(String[] args) {
+        ContestStat contest = new ContestStat();
+        
+        contest.printFirstScore();
+    }
+}
