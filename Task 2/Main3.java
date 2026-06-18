5ae81a947469ca00a1220169e25b727931f0dfa4
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-18 16:54:32 +0600

    Update Main3.java

--- Task 2/Main3.java
+++ Task 2/Main3.java
@@ -1,1 +1,20 @@
 
+package com.mycompany.main1;
+
+class CountDown{
+    void start(){
+        for(int i=5; i>=1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Go!");
+    }
+}
+public class Main3 {
+    public static void main(String[] args) {
+        
+        CountDown countdown = new CountDown();
+        
+        countdown.start();
+    }
+ 
+}
