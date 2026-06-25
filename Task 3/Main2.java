165de1b9357b4ba4f75e9351706a4b16ce3a8243
hajeramahmud <hajeraaktherhaj@gmail.com>
2026-06-25 16:48:55 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,61 @@
+package com.mycompany.lab3t2;
 
+public class Lab3t2 {
+
+    public static void main(String[] args) {
+        
+        Shape[] shapes = {
+            new Rectangle(5,4),
+            new Circle(3)
+        };
+        
+        AreaCalculator calculator = new AreaCalculator();
+        System.out.println("Total Area: " +
+                calculator.calculateTotalArea(shapes));
+    }
+}
+
+interface Shape{
+    double calculateArea();
+}
+
+class Rectangle implements Shape{
+    private  double length;
+    private double width;
+    
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+    
+    public double calculateArea(){
+        return length * width;
+    }
+}
+
+class Circle implements Shape{
+    private double radius;
+    
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    
+    public double calculateArea(){
+        return Math.PI*radius*radius;
+    }
+}
+
+
+class AreaCalculator{
+    
+    public double calculateTotalArea(Shape[] shapes){
+        
+        double totalArea = 0;
+        
+        for(Shape shape : shapes){
+            totalArea += shape.calculateArea();
+        }
+        
+        return totalArea;
+    }
+}
